5c53d21b6670cdef7fff2e6c003900f6e486c11e
Shirley Fang <fangyixuan0123@gmail.com>
2024-10-14 01:27:47 -0400

    Finish the static home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <p className="title">Shirley Fang</p>
+    </div>
+  );
+}
+
+export default HomePage;
