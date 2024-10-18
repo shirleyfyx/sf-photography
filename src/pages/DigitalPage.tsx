8a734b76c2b7f54287be434333c9179077849ec9
Shirley Fang <fangyixuan0123@gmail.com>
2024-10-18 01:50:31 -0400

    Finish the digital album page

--- src/pages/DigitalPage.tsx
+++ src/pages/DigitalPage.tsx
@@ -4,26 +4,110 @@ import '../styles/DigitalPage.css';
 import useFadeOut from '../components/useFadeOut'; 
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
-import WelcomeImage from '../assets/welcom_photo.jpg';
+import DigitalImage1 from '../assets/digital-photos/digital-photo1.jpg';
+import DigitalImage2 from '../assets/digital-photos/digital-photo2.jpg';
+import DigitalImage3 from '../assets/digital-photos/digital-photo3.jpg';
+import DigitalImage4 from '../assets/digital-photos/digital-photo4.jpg';
+import DigitalImage5 from '../assets/digital-photos/digital-photo5.jpg';
+import DigitalImage6 from '../assets/digital-photos/digital-photo6.jpg';
+import DigitalImage7 from '../assets/digital-photos/digital-photo7.jpg';
+import DigitalImage8 from '../assets/digital-photos/digital-photo8.jpg';
+import DigitalImage9 from '../assets/digital-photos/digital-photo9.jpg';
+import DigitalImage10 from '../assets/digital-photos/digital-photo10.jpg';
+import DigitalImage11 from '../assets/digital-photos/digital-photo11.jpg';
+import DigitalImage12 from '../assets/digital-photos/digital-photo12.jpg';
+import DigitalImage13 from '../assets/digital-photos/digital-photo13.jpg';
+import DigitalImage14 from '../assets/digital-photos/digital-photo14.jpg';
+import DigitalImage15 from '../assets/digital-photos/digital-photo15.jpg';
+import DigitalImage16 from '../assets/digital-photos/digital-photo16.jpg';
+import DigitalImage17 from '../assets/digital-photos/digital-photo17.jpg';
+import DigitalImage18 from '../assets/digital-photos/digital-photo18.jpg';
+import DigitalImage19 from '../assets/digital-photos/digital-photo19.jpg';
+import DigitalImage20 from '../assets/digital-photos/digital-photo20.jpg';
+import DigitalImage21 from '../assets/digital-photos/digital-photo21.jpg';
+import DigitalImage22 from '../assets/digital-photos/digital-photo22.jpg';
+import DigitalImage23 from '../assets/digital-photos/digital-photo23.jpg';
+import DigitalImage24 from '../assets/digital-photos/digital-photo24.jpg';
+import DigitalImage25 from '../assets/digital-photos/digital-photo25.jpg';
+import DigitalImage26 from '../assets/digital-photos/digital-photo26.jpg';
+import DigitalImage27 from '../assets/digital-photos/digital-photo27.jpg';
+import DigitalImage28 from '../assets/digital-photos/digital-photo28.jpg';
+import DigitalImage29 from '../assets/digital-photos/digital-photo29.jpg';
+import DigitalImage30 from '../assets/digital-photos/digital-photo30.jpg';
+import DigitalImage31 from '../assets/digital-photos/digital-photo31.jpg';
+import DigitalImage32 from '../assets/digital-photos/digital-photo32.jpg';
+import DigitalImage33 from '../assets/digital-photos/digital-photo33.jpg';
+import DigitalImage34 from '../assets/digital-photos/digital-photo34.jpg';
+import DigitalImage35 from '../assets/digital-photos/digital-photo35.jpg';
+import DigitalImage36 from '../assets/digital-photos/digital-photo36.jpg';
+import DigitalImage37 from '../assets/digital-photos/digital-photo37.jpg';
 
 const DigitalPage: React.FC = () => {
   const { fadeOut } = useFadeOut('/sf-photography', 1000); 
 
   return (
     <motion.div
-      className={`welcomepage ${fadeOut ? 'fade-out' : ''}`}
+      className={`digitalpage ${fadeOut ? 'fade-out' : ''}`}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1.4 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.8 }} 
     >
-      
       <Header />
-      
       <Sidebar />
       
-      <div className="content-container">
-        
+      <div className="image-container">
+      <img src={DigitalImage1} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage3} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage2} alt="cat" className="digital-portrait-image" />
+
+      <img src={DigitalImage25} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage35} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage31} alt="cat" className="digital-portrait-image" />
+
+      <img src={DigitalImage6} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage4} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage5} alt="cat" className="digital-portrait-image" />
+
+      <img src={DigitalImage11} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage12} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage7} alt="cat" className="digital-portrait-image" />
+      
+      <img src={DigitalImage8} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage9} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage10} alt="cat" className="digital-portrait-image" />
+
+      <img src={DigitalImage16} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage17} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage18} alt="cat" className="digital-portrait-image" />
+
+      
+      <img src={DigitalImage36} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage34} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage33} alt="cat" className="digital-portrait-image" />
+
+      <img src={DigitalImage13} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage14} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage15} alt="cat" className="digital-portrait-image" />
+
+      <img src={DigitalImage30} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage29} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage21} alt="cat" className="digital-portrait-image" />
+
+      <img src={DigitalImage22} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage28} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage19} alt="cat" className="digital-portrait-image" />
+
+      <img src={DigitalImage24} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage27} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage23} alt="cat" className="digital-portrait-image" />
+
+      <img src={DigitalImage26} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage32} alt="cat" className="digital-portrait-image" />
+      <img src={DigitalImage20} alt="cat" className="digital-portrait-image" />
+
+      {/* <img src={DigitalImage37} alt="cat" className="digital-portrait-image" /> */}
+
       </div>
     </motion.div>
   );
